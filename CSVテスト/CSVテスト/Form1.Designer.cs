93bd105d84ac26513208738aef43d038ac3f2b57
sunagat <47350501+sunagat@users.noreply.github.com>
2019-11-26 00:33:11 +0900

    Add files via upload

---
+++ CSVテスト/CSVテスト/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace CsvTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnInputSearch = new System.Windows.Forms.Button();
+            this.txtInputFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtAdditionalValue = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 41);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(424, 268);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(361, 10);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 1;
+            this.btnRead.Text = "読み込み";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.BtnRead_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(405, 315);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(486, 315);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(75, 23);
+            this.btnEnd.TabIndex = 3;
+            this.btnEnd.Text = "終了";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.BtnEnd_Click);
+            // 
+            // btnInputSearch
+            // 
+            this.btnInputSearch.Location = new System.Drawing.Point(334, 11);
+            this.btnInputSearch.Name = "btnInputSearch";
+            this.btnInputSearch.Size = new System.Drawing.Size(21, 21);
+            this.btnInputSearch.TabIndex = 5;
+            this.btnInputSearch.Text = "...";
+            this.btnInputSearch.UseVisualStyleBackColor = true;
+            this.btnInputSearch.Click += new System.EventHandler(this.BtnInputSearch_Click);
+            // 
+            // txtInputFileName
+            // 
+            this.txtInputFileName.Location = new System.Drawing.Point(88, 12);
+            this.txtInputFileName.Name = "txtInputFileName";
+            this.txtInputFileName.Size = new System.Drawing.Size(246, 19);
+            this.txtInputFileName.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "入力ファイル：";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(455, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(106, 245);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // txtAdditionalValue
+            // 
+            this.txtAdditionalValue.Location = new System.Drawing.Point(455, 41);
+            this.txtAdditionalValue.Name = "txtAdditionalValue";
+            this.txtAdditionalValue.Size = new System.Drawing.Size(53, 19);
+            this.txtAdditionalValue.TabIndex = 12;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(514, 40);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(47, 21);
+            this.btnAdd.TabIndex = 13;
+            this.btnAdd.Text = "追加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(455, 10);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(53, 21);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "クリア";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(514, 10);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(47, 21);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "削除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 350);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtAdditionalValue);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInputFileName);
+            this.Controls.Add(this.btnInputSearch);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "CSVビュワー";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnInputSearch;
+        private System.Windows.Forms.TextBox txtInputFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtAdditionalValue;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ CSVテスト/CSVテスト/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//追加
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CsvTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGridView();
+            InitializeChartArea();
+            txtInputFileName.Text = @"C:\Users\anzhir\Desktop\csv";
+        }
+
+        #region イベントハンドラ
+        private void BtnRead_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(txtInputFileName.Text))
+            {
+                List<string> list = ReadCsv();
+
+                if(dataGridView1.Rows.Count != 0)
+                {
+                    if (MessageBox.Show("入力されているデータの末端に、新たにファイル内容を追加しますか？",
+                        "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.No)
+                    {
+                        if (MessageBox.Show("現在入力されているデータを削除して新たに取り込みますか？",
+                            "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                        {
+                            dataGridView1.Rows.Clear();
+                            InitializeChartArea();
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+                //データグリッドビューへの追加
+                string inputLog = string.Empty;
+                for(int i = 0;i < list.Count;i++)
+                {
+                    if (!CheckAndAddPoint(list[i]))
+                    {
+                        inputLog += i.ToString() + ",";
+                    }
+                }
+                if(inputLog != string.Empty)
+                {
+                    MessageBox.Show("数値に変換できないデータはスキップされました。\n対象行：" + inputLog,
+                        "確認", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                txtAdditionalValue.Focus();
+            }
+            else
+            {
+                MessageBox.Show("指定したファイルが見つかりません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            string saveFileName = string.Empty;
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.InitialDirectory = @"C:\Users\anzhir\Desktop\csv";
+                sfd.FileName = "output.csv";
+                sfd.Title = "保存";
+                sfd.Filter = "CSVファイル(*.csv)|*.csv|テキストファイル(*.txt)|*.txt|すべてのファイル(*.*)|*.*";
+                sfd.RestoreDirectory = true;
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    saveFileName = sfd.FileName;
+                }
+            }
+
+            //書き込みます。
+            if(saveFileName != string.Empty)
+            {
+                //データグリッドビュー項目からCSV文字列を生成
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                {
+                    sb.Append(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                    sb.Append(",");
+                }
+                //最後の一行の追加(カンマをつけない)
+                sb.Append(dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[1].Value.ToString());
+
+                //書き込み
+                WriteCsv(saveFileName,sb.ToString());
+            }
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("プログラムを終了します。よろしいですか？",
+                        "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void BtnInputSearch_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.InitialDirectory = @"C:\Users\anzhir\Desktop\csv";
+                ofd.Title = "入力ファイル選択";
+                ofd.RestoreDirectory = true;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtInputFileName.Text = ofd.FileName;
+                }
+            }  
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            txtAdditionalValue.BackColor = Color.White;
+            if(!CheckAndAddPoint(txtAdditionalValue.Text))
+            {
+                MessageBox.Show("入力された値は無効です。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtAdditionalValue.BackColor = Color.Pink;
+            }
+            else
+            {
+                txtAdditionalValue.Text = string.Empty;
+            }
+            txtAdditionalValue.Focus();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("調整中です。",
+                        "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("クリアしますか？",
+                        "確認", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+            {
+                dataGridView1.Rows.Clear();
+                InitializeChartArea();
+            }
+        }
+
+        #endregion
+
+        #region メソッド
+        private List<string> ReadCsv()
+        {
+            //ファイル内容を格納するリスト
+            List<string> itemList = new List<string>();
+
+            string fileData = string.Empty;
+
+            //ファイル内容の全取得
+            using (StreamReader sr = new System.IO.StreamReader(txtInputFileName.Text))
+            {
+                fileData = sr.ReadToEnd();
+            }
+
+            //ファイル内容をカンマ区切りで配列に格納
+            string[] items = fileData.Split(',');
+
+            //配列→リストに変換
+            foreach(string item in items)
+            {
+                itemList.Add(item);
+            }
+
+            return itemList;            
+        }
+
+        private void WriteCsv(string outputFileName, string text)
+        {
+            //書き込みます。
+            using (StreamWriter sw = new StreamWriter(outputFileName, false, Encoding.UTF8))
+            {
+                sw.Write(text);
+            }
+            MessageBox.Show("保存が完了しました。",
+                    "確認", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void InitializeDataGridView()
+        {
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.Columns[0].HeaderText = "No.";
+            dataGridView1.Columns[1].HeaderText = "値";
+            dataGridView1.Columns[0].Width = 35;
+            dataGridView1.Columns[1].Width = 58;
+            //dataGridView1.Columns[1].Frozen = true;
+        }
+
+        public void InitializeChartArea()
+        {
+            //初期化
+            chart1.ChartAreas.Clear();
+            chart1.Series.Clear();
+            chart1.Legends.Clear();
+            chart1.ChartAreas.Add(new ChartArea("Result"));
+
+            string legend = "Graph";
+            chart1.Series.Add(legend);
+            chart1.Series[legend].ChartType = SeriesChartType.Column;
+            chart1.Series[legend].Color = Color.SlateGray;
+            chart1.Series[legend].IsValueShownAsLabel = true;
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            //chart1.ChartAreas[0].AxisX.Maximum = list.Count + 0.99;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+
+            chart1.ChartAreas[0].AxisY.Maximum = 180;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Interval = 30;
+
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Gray;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dot;
+            chart1.ChartAreas[0].AxisX.ScaleView.Position = 0.0;
+        }
+
+        private bool CheckAndAddPoint(string value)
+        {
+            //chart1.Series[0].Points.Add(x, y);
+
+            double y = 0.0;
+
+            if (Double.TryParse(value, out y))
+            {
+                dataGridView1.Rows.Add(dataGridView1.RowCount + 1, y);
+                chart1.Series[0].Points.AddXY((double)dataGridView1.RowCount, y);
+
+                //行数を更新
+                chart1.ChartAreas[0].AxisX.Maximum = dataGridView1.RowCount + 0.99;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
